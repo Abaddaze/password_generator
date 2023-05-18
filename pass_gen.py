503a4eaf4916376d22929691ae08767aea3af8de
moh.badaz558@gmail.com <moh.badaz558@gmail.com>
2023-05-18 15:01:57 +0100

    the full project

--- pass_gen.py
+++ pass_gen.py
@@ -7,10 +7,10 @@ import random
 #make a window area
 window = Tk()
 window.title("Password Generator")
-window.geometry("300x300")
+window.geometry("400x250")
 
 #create frame to assign buttons and labels to
-frame = Frame(window,width=600,height=300,bg="red")
+frame = Frame(window,width=400,height=300,bg="#454545")
 frame.pack(expand=True)
 
 #our data types for output , small chcarachters , caps , numbers , numbers in reverse order , symboles
@@ -54,11 +54,17 @@ def generate():
     #generate password from all_types variable using random.choice and for loop in range method
     generated_password = [random.choice(all_types) for i in range(pass_slider.get())]
     # join password output to make it in 1 sentence
+    global joined_pass
     joined_pass = "".join(generated_password)
     # display the output on the label textbox
     mystring.set(str(joined_pass))
     password_length=len(joined_pass)
     text_Box.config(width=password_length)
+def copy_pass ():
+    pyperclip.copy(joined_pass)
+
+    title_label = Button(frame, text="Copy", font=("Roboto", 14), bg="#039B5F", width=10, height=1, command=copy_pass)
+    title_label.place(x=240,y=170)
 #enteries and number variables
 mystring=StringVar()
 pass_text = StringVar()
@@ -71,31 +77,34 @@ pass_slider = IntVar()
 #DESIGN THE LAYOUT (Buttons and Texbox)
 #text label to show the generated password
 
-text_Box = Label(frame,justify=CENTER,textvariable=mystring,font=("courier",25),bg="#DBDBDB",fg="#039b5f",
-                 width=100)
+text_Box = Label(frame,justify=CENTER,textvariable=mystring,font=("courier",13),bg="#FF6000",fg="#212121",
+                 height=2,width=100)
 text_Box.place(relx=0.5,y=40,anchor=CENTER)
 
 #check button to select if the password has Symboles
-check_symboles = Checkbutton(frame,text="Symboles",bg="#039b5f",variable=symboles_checked,
+check_symboles = Checkbutton(frame,text="Symboles",bg="#FF6000",variable=symboles_checked,
                              onvalue=1,offvalue=0)
-check_symboles.place(x=110,y=90)
+check_symboles.place(x=40,y=65)
 
 #check button to select if the password has Numbers
-check_numbers = Checkbutton(frame,text="Numbers",bg="#039b5f",variable=numbers_checked,
+check_numbers = Checkbutton(frame,text="Numbers",bg="#FF6000",variable=numbers_checked,
                              onvalue=1,offvalue=0)
-check_numbers.place(x=210,y=90)
+check_numbers.place(x=160,y=65)
 
 #check button to select if the password has Capitals
-check_capital = Checkbutton(frame,text="Capitals",bg="#039b5f",variable=capital_checked,width=10,
+check_capital = Checkbutton(frame,text="Capitals",bg="#FF6000",variable=capital_checked,width=10,
                              onvalue=1,offvalue=0)
-check_capital.place(x=310,y=90)
+check_capital.place(x=270,y=65)
 
 #slider to choose Password Length
-pass_scale = Scale(frame, from_=0, to=24,orient=HORIZONTAL,variable=pass_slider)
-pass_scale.place(x=210,y=125)
+pass_scale = Scale(frame, from_=12, to=36,orient=HORIZONTAL,variable=pass_slider,sliderlength=20,relief=FLAT)
+pass_scale.place(relx=0.5,y=120,anchor=CENTER)
 
-title_label = Button(frame,text="GeneratePassword",font=("Roboto",14),fg="white",bg="#039b5f",
-                     width=20,height=1,command=generate)
-title_label.place(y=200, relx=0.5,anchor=CENTER)
+title_label = Button(frame,text="Generate Password",font=("Roboto",14),fg="black",bg="#FF6000",
+                     width=15,height=1,command=generate)
+title_label.place(x=30,y=170)
+
+title_label = Button(frame, text="Copy", font=("Roboto", 14), bg="#039B5F", width=10, height=1, command=copy_pass)
+title_label.place(x=240,y=170)
 #///////////////////////////////////////////////////////////////////
 window.mainloop()
